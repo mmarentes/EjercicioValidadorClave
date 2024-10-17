455a4eb51d25345586dd6462096eac5cc3a9aa98
Nzkill282 <kevincanizales07@gmail.com>
2024-10-17 11:40:41 -0500

    commit

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,25 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada_: int = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        if self.longitud_esperada_ == clave:
+            return True
+
+    def _contiene_mayusculas(self, clave: str) -> bool:
+        return clave.isupper()
+
+    def _contiene_minusculas(self, clave: str) -> bool:
+        return clave.islower()
+
+    def _contiene_numero(self, clave: str) -> bool:
+        return clave.isdigit()
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
