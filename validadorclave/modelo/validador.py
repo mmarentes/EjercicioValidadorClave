23e4633d5fb06dab5be54ff2e69abf1cc3c8604a
Nzkill282 <kevincanizales07@gmail.com>
2024-10-17 11:41:15 -0500

    commit

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -64,4 +64,51 @@ class ReglaValidacionCalisto(ReglaValidacion):
                     return True
 
 
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self, longitud_esperada: int):
+        super().__init__(longitud_esperada)
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        especiales = "@_#$%"
+        for caracter in clave:
+            if caracter in especiales:
+                return True
+
+    def es_valida(self, clave: str) -> bool:
+        super().es_valida(clave)
+        if super()._validar_longitud(clave):
+            if super()._contiene_mayusculas(clave):
+                if super()._contiene_minusculas(clave):
+                    if super()._contiene_numero(clave):
+                        if self.contiene_caracter_especial(clave):
+                            return True
+
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def __init__(self, longitud_esperada: int):
+        super().__init__(longitud_esperada)
+
+    def contiene_calisto(self, clave: str) -> bool:
+        mayusculas = 0
+        if "calisto" in clave:
+            for i in clave:
+                if i.isupper():
+                    mayusculas += 1
+        if mayusculas >= 2 and mayusculas != len(clave):
+            return True
+
+    def es_valida(self, clave: str) -> bool:
+        super().es_valida(clave)
+        if super()._validar_longitud(clave):
+            if super()._contiene_numero(clave):
+                if self.contiene_calisto(clave):
+                    return True
+
+
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        self.regla: ReglaValidacion = regla
+
+    def es_valida(self, clave: str) -> bool:
+        pass
 
